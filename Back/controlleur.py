71c41d2062330529cc15bbe1c93e83b344df308a
Yoann ABHERVE <yabherve@retis.fr>
2018-05-16 16:47:18 +0200

    update

--- Back/controlleur.py
+++ Back/controlleur.py
@@ -41,6 +41,7 @@ statusPila={
 
 
 @app.route('/')
+@crossdomain(origin='*')
 def index():
     return "Hello !"
 
@@ -84,6 +85,7 @@ def difficulte():
 		return abort(400)"""
 
 @app.route('/reception_etat', methods=['GET', 'POST'])
+@crossdomain(origin='*')
 def reception_etat():
 	session = requests.session()
 	if request.form['etat'] == 0:
@@ -100,20 +102,25 @@ def reception_etat():
 	elif request.form['etat'] == 2:
 		# besoin assistance humaine, envoi requete appli reac port 8081
 		#je ne peux pas push chez valentine, elle doit venir chercher en variable du contenu toutes les x secondes
+		
 		pila=request.form['pila']
 		etape=request.form['etape']
 		erreur=request.form['erreur']
 
 		return jsonify(
 			
-        	retour="besoin assistance humaine, donénes envoyé :",
+        	retour="besoin assistance humaine, données envoyé :",
         	data = {'pila':request.form['pila'],'etape':request.form['etape'],'erreur': request.form['erreur']}
     	), 200
 	else:
 		#return message d'erreur bad request
 		return abort(400)
 
+
+
+
 @app.route('/reponse_alerte', methods=['POST'])
+@crossdomain(origin='*')
 def reponse_alerte():
 	if request.form['reponse'] == "0":
 		#faux positif
@@ -137,6 +144,11 @@ def reponse_alerte():
 			
         	retour="reponse mal définit dans le post"
     	), 400
+
+@app.route('/pick', methods=['GET'])
+@crossdomain(origin='*')
+def pick():
+		return jsonify({'pila': pila, 'etape': etape, 'erreur': erreur}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
